3e2edf5cc566106c7803734b30d4c82aff0f104f
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2026-01-17 10:16:26 -0300

    chore: Add new lib install, lucide-chart.

--- Mokirim/src/main.tsx
+++ Mokirim/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home.tsx';
+import './index.css';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
