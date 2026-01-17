c5e0b6e1737d129682f4dcfac2c10cc0fb3d43ba
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2026-01-16 22:26:46 -0300

    chore: add React Router v6 with createBrowserRouter, create initial Home page, organize project structure by moving pages into src, fix main.tsx render configuration, and add .editorconfig for editor formatting standardization

--- Mokirim/src/main.tsx
+++ Mokirim/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
